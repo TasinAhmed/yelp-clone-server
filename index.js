eb75daf549991a49e06967fcedd866086e6fda6c
Tasin Ahmed <atasin1998@gmail.com>
2021-05-14 19:19:36 -0400

    Set cors origin and credentials to true

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use("/api/restaurants", restaurantRoutes);
 
 app.listen(port, () =>
